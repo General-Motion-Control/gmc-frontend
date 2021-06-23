daca860c7d63aba723d5e7bbdc2fc9dad8720664
daxay <daxay@scaletech.xyz>
2021-06-24 00:11:45 +0530

    feat: Added support of firebase

---
+++ assets/js/firebase-init.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+    apiKey: "AIzaSyCS0jnSsX7D0xvEue8fxP2P7dAkYGVNV-4",
+    authDomain: "general-motion-control.firebaseapp.com",
+    projectId: "general-motion-control",
+    storageBucket: "general-motion-control.appspot.com",
+    messagingSenderId: "581904581636",
+    appId: "1:581904581636:web:6fa8b4c4e38d35c6462151",
+    measurementId: "G-BFXZ8W8ZV4"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
